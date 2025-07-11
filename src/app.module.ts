729adcebf42dc9ce1aa3246cc5a304478cf25311
Pas <116931403+pasdeveloper@users.noreply.github.com>
2025-07-11 15:57:40 +0200

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
-// TODO: sposta in config module
-const dbUri =
-  'mongodb://admin:admin@localhost:27017/user_projects?authSource=admin';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, MongooseModule.forRoot(dbUri)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_DB_URI'),
+      }),
+    }),
+    UsersModule,
+    ProjectsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,13 +5,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private configService: ConfigService,
+  ) {}
 
   async createUser(createUser: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(createUser.password, 10);
+    const salt = this.configService.get<number>('BCRYPT_SALT', 10);
+    const hashedPassword = await bcrypt.hash(createUser.password, salt);
     const newUser = new this.userModel({
       ...createUser,
       password: hashedPassword,
@@ -21,7 +26,8 @@ export class UsersService {
 
   async updateUser(id: string, updateUser: UpdateUserDto) {
     if (updateUser.password) {
-      updateUser.password = await bcrypt.hash(updateUser.password, 10);
+      const salt = this.configService.get<number>('BCRYPT_SALT', 10);
+      updateUser.password = await bcrypt.hash(updateUser.password, salt);
     }
     const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUser, {
       new: true,
