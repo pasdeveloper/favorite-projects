a0bc1505c23a90a55529c66bb714f9921252e55e
Pas <116931403+pasdeveloper@users.noreply.github.com>
2025-07-11 12:09:25 +0200

    mongodb connection and prettier fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, ProjectsModule],
+  imports: [
+    UsersModule,
+    ProjectsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/mydb'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
