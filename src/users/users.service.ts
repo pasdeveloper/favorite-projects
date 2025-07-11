e3d320d353dfbe897af1ec88d160a250777bceb0
Pas <116931403+pasdeveloper@users.noreply.github.com>
2025-07-11 15:38:13 +0200

    password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,17 +4,25 @@ import { UpdateUserDto } from './models/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  createUser(createUser: CreateUserDto) {
-    const newUser = new this.userModel(createUser);
+  async createUser(createUser: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUser.password, 10);
+    const newUser = new this.userModel({
+      ...createUser,
+      password: hashedPassword,
+    });
     return newUser.save();
   }
 
   async updateUser(id: string, updateUser: UpdateUserDto) {
+    if (updateUser.password) {
+      updateUser.password = await bcrypt.hash(updateUser.password, 10);
+    }
     const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUser, {
       new: true,
     });
